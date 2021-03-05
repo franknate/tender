7ab9806749051ed8995049609968b80f03c63d86
Frank Nátán <natan.frank2000@gmail.com>
2021-03-05 11:13:51 +0100

    remove old django frontend

--- django-frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),                 
-    path('display/', views.DisplayView.as_view(), name='display'),
-    path('', views.HomeView.as_view(), name='home')
-]
